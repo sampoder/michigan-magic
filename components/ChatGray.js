95ed6f183a0b4056114f2c4f013d3ec6ed4734f4
Faisal Sayed <faisal.sayed502@gmail.com>
2023-11-18 17:26:49 -0500

    Add ChatGray component to the home page

---
+++ components/ChatGray.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const ChatGray = () => {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        background: "#E6E5EB",
+        borderRadius: 15,
+        padding: 10,
+        color: "#000",
+        color: "black",
+        fontSize: 17,
+        fontFamily: "SF Pro",
+        fontWeight: "400",
+        wordWrap: "break-word",
+      }}
+    >
+      test 123
+    </div>
+  );
+};
+
+export default ChatGray;
--- pages/api/generate-image.js
+++ pages/api/generate-image.js
@@ -1,35 +1,38 @@
-import { Configuration, OpenAIApi } from "openai";
+import { OpenAI } from "openai";
 
-const configuration = new Configuration({
-  organization: "YOUR_ORG_ID",
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAI();
 
 export default async function handler(req, res) {
   const { snippet } = req.body;
 
-  // 1. feed the snippet to gpt and generate a dalle-3 prompt
-  // 2. feed the prompt to dalle-3 and generate an image
-  // 3. return the image to the client
+  if (!snippet) {
+    return res.status(400).json({ error: "Snippet is required" });
+  }
 
-  const completion = await openai.chat.completions.create({
-    messages: [
-      {
-        role: "user",
-        content: `We are generating a role-play story where our main character is the CEO of a big AI company called OpenAI. We need to generate AI images for various scenarios in the story. The current scenario right now is: ${snippet}. Generate a DALL-E 3 prompt for this scenario.`,
-      },
-    ],
-    model: "gpt-3.5-turbo",
-  });
+  // const completion = await openai.chat.completions.create({
+  //   messages: [
+  //     {
+  //       role: "user",
+  //       content: `We are generating a role-play story where our main character is the CEO of a big AI company called OpenAI. There can be various scenarios that happen at different points of the story.
+  //        We need to generate AI images for these scenarios in the story, and those images should nicely describe the environment.
+  //        The current scenario right now is: ${snippet}.
+  //       Generate a detailed DALL-E 3 prompt for this scenario that can help us add a relevant image to the story.
+  //       Do not include any additonal text. Your response should strictly be a DALL-E 3 prompt.`,
+  //     },
+  //   ],
+  //   model: "gpt-3.5-turbo",
+  // });
 
-  const prompt = completion.choices[0].text;
+  // console.log(completion.choices[0].message);
 
   const image = await openai.images.generate({
     model: "dall-e-3",
-    prompt,
+    prompt: `We are generating a role-play story where our main character is the CEO of a big AI company called OpenAI. There can be various scenarios that happen at different points of the story.
+      We need to generate AI images for these scenarios in the story, and those images should nicely describe the environment. 
+      The current scenario right now is: ${snippet}.`,
   });
+
+  console.log(image);
 
   return res.status(200).json(image.data);
 }
