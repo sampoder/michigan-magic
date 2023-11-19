73294495965468b23d83a33b6004bf886193db23
Sam Poder <sam.r.poder@gmail.com>
2023-11-19 00:35:39 -0800

    Revert "Return file URL"
    This reverts commit 6a9a97e9720c6fe0f78c667cbac3c6e2c88cf77d.

--- api/upload.ts
+++ api/upload.ts
@@ -1,46 +1,46 @@
-import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { ListObjectsV2Command, S3Client } from "@aws-sdk/client-s3";
 import multer from "multer";
 import multerS3 from "multer-s3";
 
 const s3 = new S3Client({
-  region: process.env.C_AWS_REGION as string,
-  credentials: {
-    accessKeyId: process.env.C_AWS_ACCESS_KEY as string,
-    secretAccessKey: process.env.C_AWS_SECRET_KEY as string,
-  },
+	region: process.env.C_AWS_REGION as string,
+	credentials: {
+		accessKeyId: process.env.C_AWS_ACCESS_KEY as string,
+		secretAccessKey: process.env.C_AWS_SECRET_KEY as string,
+	},
 });
 
 const upload = multer({
-  limits: {
-    fileSize: 1024 * 1024 * 5,
-    files: 1,
-  },
-  fileFilter: (_, file, cb) => {
-    if (!file.mimetype.includes("audio")) {
-      cb(new Error("Only audio files are allowed."));
-    } else {
-      cb(null, true);
-    }
-  },
-  storage: multerS3({
-    s3: s3,
-    bucket: process.env.C_AWS_BUCKET_NAME as string,
-    key: function (req, file, cb) {
-      cb(null, file.originalname);
-    },
-  }),
+	limits: {
+		fileSize: 1024 * 1024 * 5,
+		files: 1,
+	},
+	fileFilter: (_, file, cb) => {
+		if (!file.mimetype.includes("audio")) {
+			cb(new Error("Only audio files are allowed."));
+		} else {
+			cb(null, true);
+		}
+	},
+	storage: multerS3({
+		s3: s3,
+		bucket: process.env.C_AWS_BUCKET_NAME as string,
+		metadata: function (req, file, cb) {
+			cb(null, { name: file.originalname });
+		},
+		key: function (req, file, cb) {
+			cb(null, `${Date.now().toString()}.webm`);
+		},
+	}),
 }).any();
 
-export default async (req, res) => {
-  upload(req, res, async (err) => {
-    if (err instanceof multer.MulterError) {
-      return res.status(500).send(err);
-    } else if (err) {
-      return res.status(500).send(err);
-    }
-
-    return res
-      .status(200)
-      .send(`https://${process.env.C_AWS_BUCKET_NAME}.s3.amazonaws.com/${req.file.originalname}`);
-  });
-};
+export default (req, res) => {
+	upload(req, res, function (err) {
+		if (err instanceof multer.MulterError) {
+			return res.status(500).send(err);
+		} else if (err) {
+			return res.status(500).send(err);
+		}
+		return res.status(200).send('File uploaded successfully.');
+	})
+}
